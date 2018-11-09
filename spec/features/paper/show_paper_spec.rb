91e210e374b2fd2ae673e88a98d8a22425416313
leonhard.hennicke <leonhard.hennicke@student.hpi.de>
2018-11-09 06:52:24 +0100

    Failing tests for #18.

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -9,4 +9,13 @@ describe "show paper page", type: :feature do
   it "should render without error" do
     visit paper_path(@paper)
   end
+
+  it "should list the papers authors" do
+    visit paper_path(@paper)
+
+    @paper.authors.each do |author|
+      expect(page).to have_text(author.first_name)
+      expect(page).to have_text(author.last_name)
+    end
+  end
 end
