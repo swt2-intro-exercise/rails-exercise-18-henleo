ff10caca33cdd84f5f5394da237c1426b99a901a
leonhard.hennicke <leonhard.hennicke@student.hpi.de>
2018-11-09 03:14:56 +0100

    Add failing test for issue #7.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,15 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
 
-  author = Author.new(:first_name => 'Alan', :last_name => 'Turing', :homepage => 'alan-turing.co.uk')
+  before :context do
+    @alan = FactoryBot.build(:author)
+  end
 
   it "should have these attributes" do
-    expect(author.first_name).to eq('Alan')
-    expect(author.last_name).to eq('Turing')
-    expect(author.homepage).to eq('alan-turing.co.uk')
+    expect(@alan.first_name).to eq('Alan')
+    expect(@alan.last_name).to eq('Turing')
+    expect(@alan.homepage).to eq('alan-turing.co.uk')
   end
 
   it "should have a name method" do
-    expect(author.name).to eq('Alan Turing')
+    expect(@alan.name).to eq('Alan Turing')
+  end
+
+  it "should have a last name" do
+    @peter = FactoryBot.build(:author, last_name: '')
+    expect(@peter).to_not be_valid
   end
 end
