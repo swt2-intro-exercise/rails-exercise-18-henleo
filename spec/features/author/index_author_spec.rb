cf34899925c4a3d0c94d92daae1755b277a6c853
leonhard.hennicke <leonhard.hennicke@student.hpi.de>
2018-11-09 02:41:21 +0100

    Add failing tests for issue #6.

---
+++ spec/features/author/index_author_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe "index authors page" do
+
+  before :context do
+    @alan = FactoryBot.create(:author)
+    @tim = FactoryBot.create(:author, first_name: 'Tim')
+  end
+
+  it "should render without errors" do
+    visit authors_path
+  end
+
+  it "should contain the names of all users" do
+    visit authors_path
+    expect(page).to have_text('Alan Turing')
+    expect(page).to have_text('Tim Turing')
+  end
+
+  it "should contain links to the users detail pages" do
+    visit authors_path
+    expect(page).to have_link 'Alan Turing', href: author_path(@alan)
+    expect(page).to have_link 'Tim Turing', href: author_path(@tim)
+  end
+
+  it "should contain a link to add a new author" do
+    visit authors_path
+    expect(page).to have_link 'New', href: new_author_path
+  end
+end
